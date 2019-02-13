25e5a82b050a14d8982807cfea7b6371a0bd2f35
HBLong <zhenghuanbin.it@foxmail.com>
2019-02-13 14:21:12 +0800

    daily download script

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+
+# download laster channel_v3.json
+echo "download channel_v3.json to ${SHELL_FOLDER}/channel_v3.json"
+curl -o ${SHELL_FOLDER}/channel_v3.json https://packagecontrol.io/channel_v3.json
+
+# change workdir to git
+echo "go into ${SHELL_FOLDER}"
+cd $SHELL_FOLDER
+
+# push git
+echo "ready to update remote"
+git status
+git add channel_v3.json
+git commit -m "update channel_v3"
+git push
+
+echo "done"
