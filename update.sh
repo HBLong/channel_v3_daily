ce43030fb0e037844637c36f9fb1cc043014a5c0
HBLong <zhenghuanbin.it@foxmail.com>
2019-02-14 11:42:20 +0800

    update script

--- update.sh
+++ update.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 
+# change workdir to git
+echo "go into ${SHELL_FOLDER}"
+cd $SHELL_FOLDER
+
 # up to latest
 git pull
 
 # download latest channel_v3.json
 echo "download channel_v3.json to ${SHELL_FOLDER}/channel_v3.json"
 curl -o ${SHELL_FOLDER}/channel_v3.json https://packagecontrol.io/channel_v3.json
-
-# change workdir to git
-echo "go into ${SHELL_FOLDER}"
-cd $SHELL_FOLDER
 
 # push git
 echo "ready to update remote"
